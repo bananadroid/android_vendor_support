93e24d532c0785df33f9433b6bad1b2994f2fc3c
Rizky Benggolo <travarilo@gmail.com>
2022-10-26 16:48:13 +0700

    support: Update SystemSettingsListPreference

--- src/com/banana/support/preferences/SystemSettingListPreference.java
+++ src/com/banana/support/preferences/SystemSettingListPreference.java
@@ -19,7 +19,15 @@ package com.banana.support.preferences;
 import android.content.Context;
 import android.util.AttributeSet;
 
+import android.content.Context;
+import androidx.preference.ListPreference;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
 public class SystemSettingListPreference extends ListPreference {
+
+    private boolean mAutoSummary = false;
 
     public SystemSettingListPreference(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
@@ -37,6 +45,24 @@ public class SystemSettingListPreference extends ListPreference {
     }
 
     @Override
+    public void setValue(String value) {
+        super.setValue(value);
+        if (mAutoSummary || TextUtils.isEmpty(getSummary())) {
+            setSummary(getEntry(), true);
+        }
+    }
+
+    @Override
+    public void setSummary(CharSequence summary) {
+        setSummary(summary, false);
+    }
+
+    private void setSummary(CharSequence summary, boolean autoSummary) {
+        mAutoSummary = autoSummary;
+        super.setSummary(summary);
+    }
+
+    @Override
     protected void onSetInitialValue(boolean restoreValue, Object defaultValue) {
         // This is what default ListPreference implementation is doing without respecting
         // real default value:
@@ -45,4 +71,7 @@ public class SystemSettingListPreference extends ListPreference {
         setValue(restoreValue ? getPersistedString((String) defaultValue) : (String) defaultValue);
     }
 
+    public int getIntValue(int defValue) {
+        return getValue() == null ? defValue : Integer.valueOf(getValue());
+    }
 }
